de0157234c175022b1148d0ae555fc16d2d6d433
Craig Orcutt <craigaorcutt@gmail.com>
2018-02-06 22:22:11 -0600

    added partial/control for a new score card. added factory functions to create get and delete scorecards

--- app/controller/ScoreCard.js
+++ app/controller/ScoreCard.js
@@ -3,9 +3,10 @@
 angular
 .module('score')
 .controller('ScoreCard', function($scope, ScoreCardFactory){
+    
     ScoreCardFactory.getCourseData()
     .then((data)=>{
-        console.log(data.data.golfCourse);
+        console.log(data.data.holes);
     })
     .catch((err)=>{
         console.log('err',err );
