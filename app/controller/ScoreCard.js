7d02763b2f11f96c66be40a314905f558d696161
Craig Orcutt <craigaorcutt@gmail.com>
2018-02-08 09:15:51 -0600

    added grid to score card partial. add call to golf course hole collection in FB to populate partial

--- app/controller/ScoreCard.js
+++ app/controller/ScoreCard.js
@@ -4,13 +4,12 @@ angular
 .module('score')
 .controller('ScoreCard', function($scope, ScoreCardFactory, GolfCourseFactory){
     
-    // ScoreCardFactory.getCourseData()
-    // .then((data)=>{
-    //     console.log(data.data.holes);
-    // })
-    // .catch((err)=>{
-    //     console.log('err',err );
+    $scope.CardYards = {
+        yards: ''
+    };
+    GolfCourseFactory.getHoleData()
+    .then((data)=>{
+        $scope.HoleInfo = data;
+        });
         
-    // });
-    
 });
