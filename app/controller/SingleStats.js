5b814cb5dab5f747986ca5d49ceeb6e5017267d2
Craig Orcutt <craigaorcutt@gmail.com>
2018-02-12 15:43:24 -0600

    added routing to stats page

---
+++ app/controller/SingleStats.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular
+.module('score')
+.controller('SingleStats', function($scope, ScoreCardFactory, GolfCourseFactory, $routeParams, $route){
+    console.log('heyyyy');
+    
+});
