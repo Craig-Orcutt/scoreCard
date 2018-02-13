a3ec3451ac9f59bd5a84165070a6b5510d14c51a
Craig Orcutt <craigorcutt@Craigs-MacBook-Pro.local>
2018-02-13 10:14:45 -0600

    lo dash working

--- app/controller/SingleStats.js
+++ app/controller/SingleStats.js
@@ -5,67 +5,55 @@ angular
 .controller('SingleStats', function($scope, ScoreCardFactory, GolfCourseFactory, $routeParams, $route){
     console.log('heyyyy');
 
+
     $scope.StatData = [];
-    $scope.AlbatrossData =[];
-    $scope.ParData = {
-        albatross: '',
-        eagle: '',
-        birdie: '',
-        par: '',
-        bogie: '',
-        doubleBogie: '',
-        bad: ''
-    };
-    $scope.BirdieData = [];
 
-    // ScoreCardFactory.getSavedScore($routeParams.id)
-    //     .then(({data})=>{
-    //         console.log('data', data);
-    //         data.forEach((element)=>{
-    //             if(element.score === element.par -2 ){
-    //                 $scope.AlbatrossData.push(element.par);
-    //             }else if(element.score === element.par -1){
-    //                 $scope.BirdieData.push(element.par);
-    //             }
-    //             else if(element.score === element.par);
-    //                 $scope.ParData.par += `${element.score} ,`  ;
-    //         });
 
-    //     });
+    // switch case to sort pars birdies bogies etc..
+    function SwitchData(data) {
+        data.forEach((element)=>{
+            switch(element.score){
+                case element.par -3:
+                $scope.StatData.push('albatross');
+                break;
 
-    
+                case element.par -2:
+                $scope.StatData.push(`eagle`);
+                break;
+
+                case element.par -1:
+                $scope.StatData.push(`birdie`);
+                break;
+
+                case element.par: 
+                $scope.StatData.push(`par`);
+                break;
+
+                case element.par +1:
+                $scope.StatData.push(`bogie`);
+                break;
+
+                case element.par +2:
+                $scope.StatData.push(`doubleBogie`);
+                break;
+
+                default:
+                $scope.StatData.push(`bad`);
+                break;
+            }
+        });
+    }
+
+    // gets scores for current scorecard
         ScoreCardFactory.getSavedScore($routeParams.id)
             .then(({data})=>{
                 console.log('data', data);
-                data.forEach((element)=>{
-                    switch(element.score){
-                        case element.par -3:
-                        $scope.ParData.albatross += element.score;
-                        break;
-                        case element.par -2:
-                        $scope.ParData.eagle += element.score;
-                        break;
-                        case element.par -1:
-                        $scope.ParData.birdie += element.score;
-                        break;
-                        case element.par: 
-                        $scope.ParData.par += element.score;
-                        break;
-                        case element.par +1:
-                        $scope.ParData.bogie += element.score;
-                        break;
-                        case element.par +2:
-                        $scope.ParData.doubleBogie += element.score;
-                        break;
-                        default:
-                        $scope.ParData.bad += element.score;
-                        break;
-                    }
-            });
-            console.log('statdat', $scope.ParData);
+               new SwitchData(data);
+            
+            console.log('statdat', $scope.StatData);
+            $scope.LoCount = _.countBy($scope.StatData);
+            console.log('LoCount', $scope.LoCount);
         });
-        let hapDat = _.countBy($scope.ParData);
-        console.log('hap', hapDat);
         
         
 });
